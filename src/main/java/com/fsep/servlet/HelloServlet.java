297304b14feb489aace59e7af196b54b81896bf6
Miteri <raaf98@gmail.com>
2016-10-01 14:11:47 +0300

    add console output

--- src/main/java/com/fsep/servlet/HelloServlet.java
+++ src/main/java/com/fsep/servlet/HelloServlet.java
@@ -1,7 +1,5 @@
 package com.fsep.servlet;
 
-import com.fsep.servlet.utils.HTML;
-
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -18,6 +16,9 @@ public class HelloServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String name = new String(req.getParameter("name").getBytes("ISO-8859-1"),"UTF-8");
+        name = name == null || name.length() == 0 ? "стесняшка" : name.trim();
+        System.out.println("Имя пользователя: " + name);
         getServletContext().getRequestDispatcher("/WEB-INF/jsp/hello.jsp").forward(req, resp);
     }
 }
